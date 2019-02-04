ec2833fc9313917e33c151f35fc5a5007b823ea4
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2019-02-05 05:55:58 +0900

    Prepare release

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  if ENV['BASIC_AUTH_USERNAME'] && ENV['BASIC_AUTH_PASSWORD']
+    http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD']
+  end
 end
--- config/puma.rb
+++ config/puma.rb
@@ -33,3 +33,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end
